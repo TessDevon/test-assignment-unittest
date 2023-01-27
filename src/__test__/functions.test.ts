f36ffc0f99dbc15ce5a0b436ab2bcd56f57eefc4
Therese <therese.holen22@gmail.com>
2023-01-27 11:36:49 +0100

    /feat Added all tests to function.ts.

---
+++ src/__test__/functions.test.ts
@@ -0,0 +1,75 @@
+/**
+ * @jest-environment jsdom
+ */
+import { addTodo, changeTodo, removeAllTodos } from "../ts/functions";
+import { Todo } from "../ts/models/Todo";
+
+beforeEach(() => {
+    document.body.innerHTML = "";
+});
+
+describe("addTodo", () => {
+    test ("addTodoTrue", () => {
+        //Arrage
+        let todoExample = "baka"
+        let todos: Todo[] = [];
+        let length = todos.length;
+    
+        //Act
+        addTodo(todoExample, todos);
+
+        //Assert
+        expect(todos.length).toBe(length+1);
+        expect(todos[todos.length-1].text).toBe("baka")
+    });
+
+    test ("addTodoFalse", () => {
+        //Arrage
+        let todoExample = ""
+        let todos: Todo[] = [];
+        let length = todos.length;
+    
+        //Act
+        addTodo(todoExample, todos);
+
+        //Assert
+        expect(todos.length).toBe(length);
+    });
+});
+
+describe("changeTodo", () => {
+    test ("changeTodoDoneToNotdone", () => {
+        //Arrage
+        let todoExampleTrue = ("baka");
+        let changeToFalse = new Todo(todoExampleTrue, true);
+
+        //Act 
+        changeTodo(changeToFalse);
+
+        //Assert 
+        expect(changeToFalse.done).toBe(false);
+    });
+
+    test ("changeTodoNotdoneToDone", () => {
+        //Arrage
+        let todoExampleFalse = ("baka");
+        let changeToTrue = new Todo(todoExampleFalse, false);
+
+        //Act 
+        changeTodo(changeToTrue);
+
+        //Assert 
+        expect(changeToTrue.done).toBe(true);
+    });
+});
+
+test ("removeAllTodos",() => {
+    //Arrage
+    let todos: Todo[] = [new Todo("Baka", true), new Todo("Handla", true)];
+
+    //Act 
+    removeAllTodos(todos);
+
+    //Assert
+    expect(todos.length).toBe(0);
+});
